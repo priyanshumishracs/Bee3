f32568fa535c71b1bd0f9f841bd6babcbfb7af32
Priyanshu mishra <119335221+priyanshumishracs@users.noreply.github.com>
2025-09-02 15:27:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,20 +10,21 @@ terraform {
       version = "~> 3.5"
     }
   }
-}
-backend "azurerm" {
-    resource_group_name  = "mohitRG"      # RG where storage account exists
-    storage_account_name = "teststorageaccount6267"     # Your storage account
-    container_name       = "pm-tf"              # Container to hold state
-    key                  = "terraform.tfstate"    # State file name
+
+  backend "azurerm" {
+    resource_group_name  = "mohitRG"                # RG where storage account exists
+    storage_account_name = "teststorageaccount6267" # Your storage account
+    container_name       = "pm-tf"                  # Container to hold state
+    key                  = "terraform.tfstate"     # State file name
   }
+}
+
 provider "azurerm" {
   features {
     virtual_machine {
       delete_os_disk_on_deletion = true
     }
-    
   }
-  # Credentials will come from Jenkins environment variables
+  # Credentials from Jenkins env variables:
   # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
